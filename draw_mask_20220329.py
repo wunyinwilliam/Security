e75152e82de3fd5f10401905b84a2471e4f166c8
Will_Nano <wunyinwilliam@gmail.com>
2022-03-29 10:56:45 +0800

    Draw mask add buttons

--- draw_mask_20220329.py
+++ draw_mask_20220329.py
@@ -6,6 +6,8 @@ import argparse
 import sys
 
 EDGE_PIXEL = 20
+DRAW_SIZE = 10
+BOTTOM_BAR_HEIGHT = 40
 
 
 class DrawMask:
@@ -14,22 +16,25 @@ class DrawMask:
         self.is_drawing = False             # true if mouse is pressed
         self.ix, self.iy = -1, -1
         self.input = jetson.utils.videoSource(input_URI, argv)
-        self.is_white_image_created = False
         cv2.namedWindow('Draw Masks')
         cv2.setMouseCallback('Draw Masks', self.draw_circle)
+        img = cv2.cvtColor(jetson.utils.cudaToNumpy(self.input.Capture()), cv2.COLOR_BGR2RGB)
+        self.draw_img = cv2.imread('draw_mask.png')
+        if self.draw_img is None:
+            self.draw_img = self.reset_image(img.shape)
+        self.bottom_bar = self.create_bottom_bar(img.shape)
         self.start()
 
-    def create_white_image(self, shape):
+    def reset_image(self, shape):
         height, width, _ = shape
-        edges = [[0, 0, EDGE_PIXEL, height-1],
-                [0, 0, width-1, EDGE_PIXEL],
-                [0, height-EDGE_PIXEL, width-1, height-1],
-                [width-EDGE_PIXEL, 0, width-1, height-1]]
+        edges = [[0, 0, EDGE_PIXEL, height],
+                [0, 0, width, EDGE_PIXEL],
+                [0, height-EDGE_PIXEL, width, height],
+                [width-EDGE_PIXEL, 0, width, height]]
         image = np.zeros((height, width, 3), np.uint8)        # Create a white image with same dimension
         image[:] = (255, 255, 255)
         self.draw_boundary(image, edges)
         cv2.imwrite('draw_mask.png', image)
-        self.is_white_image_created = True
         return image
 
     def draw_boundary(self, frame, boxes):
@@ -37,28 +42,39 @@ class DrawMask:
             mask = np.full((box[3]-box[1], box[2]-box[0], 3), 0).astype(np.uint8)
             frame[box[1]:box[3], box[0]:box[2]] = mask
 
+    def create_bottom_bar(self, shape):
+        _, width, _ = shape
+        image = np.zeros((BOTTOM_BAR_HEIGHT, width, 3), np.uint8)
+        image[:] = (255, 255, 255)
+        cv2.line(image, (int(width/3), 0), (int(width/3), BOTTOM_BAR_HEIGHT), (0, 0, 0), 1)
+        cv2.line(image, (int(width*2/3), 0), (int(width*2/3), BOTTOM_BAR_HEIGHT), (0, 0, 0), 1)
+        cv2.putText(image, 'Clear All', (10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+        cv2.putText(image, 'Undo', (int(width/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+        cv2.putText(image, 'Eraser', (int(width*2/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+        return image
+
     # mouse callback function
     def draw_circle(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.is_drawing = True
-            self.ix, self.iy = x, y
+            # self.ix, self.iy = x, y
         elif event == cv2.EVENT_MOUSEMOVE:
             cv2.setWindowTitle("Draw Masks", f"({x},{y})")
             if self.is_drawing:
-                cv2.rectangle(self.draw_img, (self.ix, self.iy), (x, y), (0, 0, 0), -1)
+                # cv2.rectangle(self.draw_img, (self.ix, self.iy), (x, y), (0, 0, 0), -1)
+                cv2.rectangle(self.draw_img, (int(x-DRAW_SIZE/2), int(y-DRAW_SIZE/2)), (int(x+DRAW_SIZE/2), int(y+DRAW_SIZE/2)), (0, 0, 0), -1)
         elif event == cv2.EVENT_LBUTTONUP:
             self.is_drawing = False
-            cv2.rectangle(self.draw_img, (self.ix, self.iy), (x, y), (0, 0, 0), -1)
+            # cv2.rectangle(self.draw_img, (self.ix, self.iy), (x, y), (0, 0, 0), -1)
             cv2.imwrite('draw_mask.png', self.draw_img)
 
     def start(self):
         while True:
             img = cv2.cvtColor(jetson.utils.cudaToNumpy(self.input.Capture()), cv2.COLOR_BGR2RGB)
-            if not self.is_white_image_created:
-                self.draw_img = self.create_white_image(img.shape)
             combine_image = cv2.bitwise_and(img, self.draw_img)
-            cv2.imshow('Draw Masks', combine_image)
-            if cv2.waitKey(20) & 0xFF == 27:
+            combine_image_with_bottom_bar = np.concatenate((combine_image, self.bottom_bar), axis=0)
+            cv2.imshow('Draw Masks', combine_image_with_bottom_bar)
+            if cv2.waitKey(1) & 0xFF == 27:
                 cv2.destroyAllWindows()
                 sys.exit(0)
 
--- test.py
+++ test.py
@@ -1,29 +1,6 @@
-import collections
+from os.path import expanduser
+import datetime
 
-x = collections.deque([], 10)
-x.append({
-    "value": "A",
-    "frame": 3
-})
-x.append({
-    "value": "B",
-    "frame": 3
-})
-
-for i in range(5):
-    x.append({
-        "value": "C",
-        "frame": 10
-    })
-    x.append({
-        "value": "D",
-        "frame": 10
-    })
-    pop_times = 0
-    for pos in range(len(x)):
-        x[pos]["frame"] -= 1
-        if x[pos]["frame"] <= 0:
-            pop_times += 1
-    for _ in range(pop_times):
-        x.popleft()
-    print(list(x))
+file = open(expanduser("~") + '/Desktop/HERE.txt', 'w')
+file.write("It worked!\n" + str(datetime.datetime.now()))
+file.close()
