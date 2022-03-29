2fd9045e852df185165f70f236aae02c143cffb4
Will_Nano <wunyinwilliam@gmail.com>
2022-03-29 11:54:35 +0800

    Add comments

--- draw_mask_20220329.py
+++ draw_mask_20220329.py
@@ -6,26 +6,39 @@ import argparse
 import sys
 
 EDGE_PIXEL = 20
-DRAW_SIZE = 10
+DRAW_SIZE = 10              # For Draw Mode 2, each mouse click is 10x10 pixels
 BOTTOM_BAR_HEIGHT = 40
 
 
 class DrawMask:
 
     def __init__(self, input_URI, argv):
-        self.is_drawing = False             # true if mouse is pressed
-        self.draw_mode = 1
-        self.is_eraser = False
-        self.ix, self.iy = -1, -1
+        self.is_drawing = False             # True if mouse is pressed on the video
+        self.draw_mode = 1                  # Draw mode 1 is for large area, Draw mode 2 is for small area
+        self.is_eraser = False              # True if it is in eraser mode, false if normal draw mode
         self.input = jetson.utils.videoSource(input_URI, argv)
         cv2.namedWindow('Draw Masks')
-        cv2.setMouseCallback('Draw Masks', self.draw_circle)
-        img = cv2.cvtColor(jetson.utils.cudaToNumpy(self.input.Capture()), cv2.COLOR_BGR2RGB)
-        self.draw_img = cv2.imread('draw_mask.png')
+        cv2.setMouseCallback('Draw Masks', self.draw)
+        img = self.read_cuda_image()                            # Read cuda image once to obtain the shape
+        self.draw_img = cv2.imread('draw_mask.png')             # Read previous draw_mask image
         if self.draw_img is None:
             self.draw_img = self.reset_image(img.shape)
         self.bottom_bar = self.create_bottom_bar(img.shape)
         self.start()
+
+    def read_cuda_image(self):
+        # load the image from CUDA memory
+        rgb_img = self.input.Capture()
+        # convert to BGR, since that's what OpenCV expects
+        bgr_img = jetson.utils.cudaAllocMapped(width=rgb_img.width,
+                    height=rgb_img.height,
+                    format='bgr8')
+        jetson.utils.cudaConvertColor(rgb_img, bgr_img)
+        # Make sure the GPU is done work before we convert to cv2
+        jetson.utils.cudaDeviceSynchronize()
+        # convert to cv2 image (cv2 images are numpy arrays)
+        img = jetson.utils.cudaToNumpy(bgr_img)
+        return img
 
     def reset_image(self, shape):
         height, width, _ = shape
@@ -33,13 +46,14 @@ class DrawMask:
                 [0, 0, width, EDGE_PIXEL],
                 [0, height-EDGE_PIXEL, width, height],
                 [width-EDGE_PIXEL, 0, width, height]]
-        image = np.zeros((height, width, 3), np.uint8)        # Create a white image with same dimension
+        # Create a white image with same dimension
+        image = np.zeros((height, width, 3), np.uint8)        
         image[:] = (255, 255, 255)
-        self.draw_boundary(image, edges)
+        self.draw_edges(image, edges)
         cv2.imwrite('draw_mask.png', image)
         return image
 
-    def draw_boundary(self, frame, boxes):
+    def draw_edges(self, frame, boxes):
         for box in boxes:
             mask = np.full((box[3]-box[1], box[2]-box[0], 3), 0).astype(np.uint8)
             frame[box[1]:box[3], box[0]:box[2]] = mask
@@ -48,6 +62,7 @@ class DrawMask:
         _, width, _ = shape
         image = np.zeros((BOTTOM_BAR_HEIGHT, width, 3), np.uint8)
         image[:] = (255, 255, 255)
+        # Draw lines to separate 3 buttons
         cv2.line(image, (int(width/3), 0), (int(width/3), BOTTOM_BAR_HEIGHT), (0, 0, 0), 1)
         cv2.line(image, (int(width*2/3), 0), (int(width*2/3), BOTTOM_BAR_HEIGHT), (0, 0, 0), 1)
         cv2.putText(image, 'Clear All', (10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
@@ -56,13 +71,16 @@ class DrawMask:
         return image
 
     # mouse callback function
-    def draw_circle(self, event, x, y, flags, param):
+    def draw(self, event, x, y, flags, param):
         height, width, _ = self.draw_img.shape
         if event == cv2.EVENT_LBUTTONDOWN:
-            if y > height:                                          # In the Bottom Bar
-                if x < int(width/3):                                # Clear All
+            # Mouse click in the Bottom Bar
+            if y > height:         
+                # Clear All Button                                 
+                if x < int(width/3):
                     self.draw_img = self.reset_image(self.draw_img.shape)
-                elif x > int(width/3) and x < int(width*2/3):       # Undo
+                # Draw Mode Button                                 
+                elif x > int(width/3) and x < int(width*2/3):
                     if self.draw_mode == 1:
                         cv2.rectangle(self.bottom_bar, (int(width/3)+1, 0), (int(width*2/3)-1, BOTTOM_BAR_HEIGHT), (255, 255, 255), -1)
                         cv2.putText(self.bottom_bar, 'Draw Mode 2', (int(width/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
@@ -71,7 +89,8 @@ class DrawMask:
                         cv2.rectangle(self.bottom_bar, (int(width/3)+1, 0), (int(width*2/3)-1, BOTTOM_BAR_HEIGHT), (255, 255, 255), -1)
                         cv2.putText(self.bottom_bar, 'Draw Mode 1', (int(width/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
                         self.draw_mode = 1
-                elif x > int(width*2/3):                            # Eraser
+                # Eraser Button
+                elif x > int(width*2/3):
                     if not self.is_eraser:
                         cv2.rectangle(self.bottom_bar, (int(width*2/3)+1, 0), (width, BOTTOM_BAR_HEIGHT), (0, 0, 0), -1)
                         cv2.putText(self.bottom_bar, 'Eraser ON', (int(width*2/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
@@ -79,6 +98,7 @@ class DrawMask:
                         cv2.rectangle(self.bottom_bar, (int(width*2/3)+1, 0), (width, BOTTOM_BAR_HEIGHT), (255, 255, 255), -1)
                         cv2.putText(self.bottom_bar, 'Eraser OFF', (int(width*2/3)+10, BOTTOM_BAR_HEIGHT-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
                     self.is_eraser = not self.is_eraser
+            # Mouse click in the image
             else:
                 self.is_drawing = True
                 self.ix, self.iy = x, y
@@ -102,7 +122,7 @@ class DrawMask:
 
     def start(self):
         while True:
-            img = cv2.cvtColor(jetson.utils.cudaToNumpy(self.input.Capture()), cv2.COLOR_BGR2RGB)
+            img = self.read_cuda_image()
             combine_image = cv2.bitwise_and(img, self.draw_img)
             combine_image_with_bottom_bar = np.concatenate((combine_image, self.bottom_bar), axis=0)
             cv2.imshow('Draw Masks', combine_image_with_bottom_bar)
