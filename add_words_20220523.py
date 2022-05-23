f8f5114aae60660f569bda4336570fa2b9e01f51
Will_Nano <wunyinwilliam@gmail.com>
2022-05-23 18:21:02 +0800

    can change textsize in add_word

--- add_words_20220523.py
+++ add_words_20220523.py
@@ -29,6 +29,7 @@ class AddWords:
         self.is_typing = False
         self.is_delete = False
         self.is_white = True
+        self.text_size = 2
         self.text_input = ''
         self.text_location = None
         self.text_pointer_location_x = 0
@@ -77,16 +78,17 @@ class AddWords:
     def create_description_text(self, image):
         _, width, _ = image.shape
         # crop the sub-rect for text from the image
-        x, y, w, h = width-300, 0, 300, 130
+        x, y, w, h = width-300, 0, 300, 150
         sub_img = image[y:y+h, x:x+w]
         white_rect = np.ones(sub_img.shape, dtype=np.uint8) * 255
         res = cv2.addWeighted(sub_img, 0.5, white_rect, 0.5, 1.0)
-        cv2.putText(res, 'Double Click: Start Typing/Delete', (0, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'Double Click: Start Typing', (0, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
         cv2.putText(res, 'Enter: Finish Typing & Save', (0, 40), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(res, 'Clear All: F1', (0, 60), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(res, 'Normal/Delete Mode: F2', (0, 80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(res, 'White/Black Text: F3', (0, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-        cv2.putText(res, 'Exit: Esc', (0, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'White/Black Text: F1', (0, 60), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'Text Size: + / -', (0, 80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'Delete + Double Click: Remove', (0, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'Clear All: F12', (0, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+        cv2.putText(res, 'Exit: Esc', (0, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
         # Putting the sub-rect back to its position
         image[y:y+h, x:x+w] = res
         return image
@@ -104,11 +106,11 @@ class AddWords:
                 if self.is_white:
                     self.image_without_new_words = self.word_img_white.copy()
                     self.word_img_without_pointer = self.word_img_white.copy()
-                    cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                    cv2.rectangle(self.word_img_white, (self.text_location[0], self.text_location[1]-self.text_size*15), (self.text_location[0]+1, self.text_location[1]+10), (255, 255, 255), -1)
                 else:
                     self.image_without_new_words = self.word_img_black.copy()
                     self.word_img_without_pointer = self.word_img_black.copy()
-                    cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                    cv2.rectangle(self.word_img_black, (self.text_location[0], self.text_location[1]-self.text_size*15), (self.text_location[0]+1, self.text_location[1]+10), (255, 255, 255), -1)
                 self.text = 'Enter Words'
                 self.dismiss_count = DISMISS_COUNT
             else:       # Delete
@@ -123,9 +125,9 @@ class AddWords:
                 self.word_history = temp_word_history.copy()
                 for word_history in self.word_history:
                     if word_history[3]:
-                        cv2.putText(self.word_img_white, word_history[0], word_history[1], cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_white, word_history[0], word_history[1], cv2.FONT_HERSHEY_PLAIN, word_history[4], (255, 255, 255), 2)
                     else:
-                        cv2.putText(self.word_img_black, word_history[0], word_history[1], cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_black, word_history[0], word_history[1], cv2.FONT_HERSHEY_PLAIN, word_history[4], (255, 255, 255), 2)
                 self.save_word_img()
 
 
@@ -135,11 +137,11 @@ class AddWords:
                 if self.is_white:
                     self.word_img_white = self.word_img_without_pointer.copy()
                     if self.text_input != '' and self.text_location is not None and self.text_pointer_location_x != 0:
-                        self.word_history.append((self.text_input, self.text_location, self.text_pointer_location_x, True))
+                        self.word_history.append((self.text_input, self.text_location, self.text_pointer_location_x, True, self.text_size))
                 else:
                     self.word_img_black = self.word_img_without_pointer.copy()
                     if self.text_input != '' and self.text_location is not None and self.text_pointer_location_x != 0:
-                        self.word_history.append((self.text_input, self.text_location, self.text_pointer_location_x, False))
+                        self.word_history.append((self.text_input, self.text_location, self.text_pointer_location_x, False, self.text_size))
             self.save_word_img()
         else:
             if self.is_white:
@@ -185,39 +187,35 @@ class AddWords:
                 print('You pressed %d (0x%x), LSB: %d (%s)' % (key, key, key % 256,
                 repr(chr(key%256)) if key%256 < 128 else '?'))
 
-            if (key > 31) and (key < 127):        # letters or integers
+            if (key > 31) and (key < 127) and (key != 43) and (key != 45):        # letters or integers
                 if self.is_typing:
+                    self.text_input += chr(key%256) if key%256 < 128 else '?' 
+                    self.text_pointer_location_x = len(self.text_input) * self.text_size * 10
                     if self.is_white:
                         self.word_img_white = self.image_without_new_words.copy()
-                        self.text_input += chr(key%256) if key%256 < 128 else '?' 
-                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
                         self.word_img_without_pointer = self.word_img_white.copy()
-                        self.text_pointer_location_x += 20 
-                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
                     else:
                         self.word_img_black = self.image_without_new_words.copy()
-                        self.text_input += chr(key%256) if key%256 < 128 else '?' 
-                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
                         self.word_img_without_pointer = self.word_img_black.copy()
-                        self.text_pointer_location_x += 20 
-                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
 
-            elif key == 8:            # Backspace
+            elif key == 8:                       # Backspace
                 if self.is_typing and len(self.text_input) > 0:
+                    self.text_input = self.text_input[:-1]
+                    self.text_pointer_location_x = len(self.text_input) * self.text_size * 10
                     if self.is_white:
                         self.word_img_white = self.image_without_new_words.copy()
-                        self.text_input = self.text_input[:-1]
-                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
                         self.word_img_without_pointer = self.word_img_white.copy()
-                        self.text_pointer_location_x -= 20 
-                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
                     else:
                         self.word_img_black = self.image_without_new_words.copy()
-                        self.text_input = self.text_input[:-1]
-                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
                         self.word_img_without_pointer = self.word_img_black.copy()
-                        self.text_pointer_location_x -= 20 
-                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-30), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
                     self.text = 'Backspace'
                     self.dismiss_count = DISMISS_COUNT
 
@@ -227,16 +225,53 @@ class AddWords:
                     self.text = 'Enter'
                     self.dismiss_count = DISMISS_COUNT
 
-            elif key == 190:          # F1
-                self.reset_image(self.word_img_white.shape, True)
-                self.reset_image(self.word_img_black.shape, False)
+            elif key == 190:          # F1: White/Black Text
                 if self.is_typing:
                     self.stop_typing(True)
-                self.is_delete = False
-                self.text = 'Clear All'
+                self.is_white = not self.is_white
+                if self.is_white:
+                    self.text = 'White Text'
+                else:
+                    self.text = 'Black Text'
                 self.dismiss_count = DISMISS_COUNT
 
-            elif key == 191:          # F2
+            elif key == 43:           # +: Increase Text Size
+                if self.text_size < 5:
+                    self.text_size += 1
+                self.text_pointer_location_x = len(self.text_input) * self.text_size * 10
+                if self.is_typing:
+                    if self.is_white:
+                        self.word_img_white = self.image_without_new_words.copy()
+                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
+                        self.word_img_without_pointer = self.word_img_white.copy()
+                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                    else:
+                        self.word_img_black = self.image_without_new_words.copy()
+                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
+                        self.word_img_without_pointer = self.word_img_black.copy()
+                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                self.text = f'Text Size: {self.text_size}'
+                self.dismiss_count = DISMISS_COUNT
+
+            elif key == 45:           # -: Decrease Text Size
+                if self.text_size > 1:
+                    self.text_size -= 1
+                self.text_pointer_location_x = len(self.text_input) * self.text_size * 10
+                if self.is_typing:
+                    if self.is_white:
+                        self.word_img_white = self.image_without_new_words.copy()
+                        cv2.putText(self.word_img_white, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
+                        self.word_img_without_pointer = self.word_img_white.copy()
+                        cv2.rectangle(self.word_img_white, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                    else:
+                        self.word_img_black = self.image_without_new_words.copy()
+                        cv2.putText(self.word_img_black, self.text_input, self.text_location, cv2.FONT_HERSHEY_PLAIN, self.text_size, (255, 255, 255), 2)
+                        self.word_img_without_pointer = self.word_img_black.copy()
+                        cv2.rectangle(self.word_img_black, (self.text_location[0]+self.text_pointer_location_x, self.text_location[1]-self.text_size*15), (self.text_location[0]+self.text_pointer_location_x+1, self.text_location[1]+10), (255, 255, 255), -1)
+                self.text = f'Text Size: {self.text_size}'
+                self.dismiss_count = DISMISS_COUNT
+
+            elif key == 255:          # Delete: Normal/Delete Mode
                 if self.is_typing:
                     self.stop_typing(True)
                 self.is_delete = not self.is_delete
@@ -247,14 +282,13 @@ class AddWords:
                     self.text = 'Normal Mode'
                     self.dismiss_count = DISMISS_COUNT
 
-            elif key == 192:          # F3
+            elif key == 201:          # F12: Clear All
+                self.reset_image(self.word_img_white.shape, True)
+                self.reset_image(self.word_img_black.shape, False)
                 if self.is_typing:
                     self.stop_typing(True)
-                self.is_white = not self.is_white
-                if self.is_white:
-                    self.text = 'White Text'
-                else:
-                    self.text = 'Black Text'
+                self.is_delete = False
+                self.text = 'Clear All'
                 self.dismiss_count = DISMISS_COUNT
 
             elif key == 27:          # Esc
